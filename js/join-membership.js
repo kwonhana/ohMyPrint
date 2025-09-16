62bb7d38e38d201541ff5958c38465ee6cd7ad36
ukum <97ukum@gmail.com>
2025-09-16 18:36:07 +0900

    회원가입-자바스크립트

--- js/join-membership.js
+++ js/join-membership.js
@@ -15,3 +15,34 @@ select.addEventListener('change', function () {
 
   console.log(domain);
 });
+
+//-------------------비밀번호-------------------
+
+const pw = document.querySelector('#uPass');
+console.log(pw);
+
+const pwCheck = document.querySelector('#uPass-check');
+console.log(pwCheck);
+
+const pwComment = document.querySelector('.uPass-comment');
+console.log(pwComment);
+
+const pwCommentCheck = document.querySelector('.uPass-comment-check');
+
+function checkPasswordMatch() {
+  if (pw.value === '' || pwCheck.value === '') {
+    pwComment.style.display = 'none';
+  } else if (pw.value === pwCheck.value) {
+    pwCommentCheck.style.display = 'block';
+    pwComment.style.display = 'none';
+  } else {
+    pwComment.style.display = 'block';
+    pwCommentCheck.style.display = 'none';
+  }
+}
+
+// 이벤트 줄거야 input할때 위에 설정한 함수를 실행
+pw.addEventListener('input', checkPasswordMatch);
+pwCheck.addEventListener('input', checkPasswordMatch);
+
+//change
