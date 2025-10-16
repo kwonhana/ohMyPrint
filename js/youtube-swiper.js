94d7c481c0e53cce1ff0bdc2c71e161b978d0333
kwon hana <vvvhana@gmail.com>
2025-10-16 18:41:23 +0900

    style: youtube-swiper 반응형 수정

--- js/youtube-swiper.js
+++ js/youtube-swiper.js
@@ -1,18 +1,34 @@
-
-
- var swiper = new Swiper('.youtube-wrap', {
+var swiper = new Swiper('.youtube-wrap', {
+          slidesPerView: 4.5,
+          // 사이 간격
+          spaceBetween: 100,
+          centeredSlides: true,
           loop: true,
+          speed: 1000,
+
           navigation: {
             // < > 버튼
             nextEl: '.swiper-button-next',
             prevEl: '.swiper-button-prev',
           },
-          //   autoplay: {
-          //     delay: 3000,
-          //   },
-          speee: 5000,
-          slidesPerView: 4.5,
-          // 사이 간격
-          spaceBetween: 100,
-          centeredSlides: true,
-        });
+          breakpoints: {
+            0: {
+              slidesPerView: 2,
+              spaceBetween: 100,
+            },
+            768: {
+              slidesPerView: 3,
+              spaceBetween: 100,
+            },
+            1020: {
+              slidesPerView: 3,
+              spaceBetween: 100,
+            },
+            1021: {
+              slidesPerView: 4.5,
+              spaceBetween: 100,
+            }
+}
+        }
+      );
+
