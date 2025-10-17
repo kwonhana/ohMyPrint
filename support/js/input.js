7b769798864e4c9f2bdab9e12776d784fd9b3338
kwon hana <kwon hana>
2025-10-17 14:17:14 +0900

    feat: toast-ui add

--- support/js/input.js
+++ support/js/input.js
@@ -6,7 +6,8 @@ export function validateData(el, has, message) {
   el.addEventListener('change', (e) => {
     const value = e.target.value;
     if (has.test(value)) {
-      alert(`${message}는 입력할 수 없습니다.`);
+      showToast(`${message}는 입력할 수 없습니다.`);
+      console.log('동작');
       e.target.value = '';
     }
   });
@@ -17,14 +18,29 @@ export function valiDatePhone(el, num, message) {
     const value = e.target.value;
 
     if (value.length <= num) {
-      alert(message);
+      showToast(`${message}`);
       e.target.value = '';
     } else if (hasKorean.test(value)) {
-      alert(message);
+      showToast(`${message}`);
       e.target.value = '';
     } else if (hasEnglish.test(value)) {
-      alert(message);
+      showToast(`${message}`);
       e.target.value = '';
     }
+  });
+}
+
+function showToast(message) {
+  const container = document.getElementById('toast-container');
+
+  const toast = document.createElement('div');
+  toast.className = 'toast-modal';
+  toast.textContent = message;
+
+  container.appendChild(toast);
+
+  // 애니메이션이 끝나면 DOM에서 제거
+  toast.addEventListener('animationend', () => {
+    toast.remove();
   });
 }
